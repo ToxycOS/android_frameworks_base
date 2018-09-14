9ec91b87a4a4ed1c50c95944c642fbeb7ed6c604
maxwen <max.weninger@gmail.com>
2018-09-14 17:34:30 -0500

    Store Toxyc fingerprint for package data
    and use that to clear package cache instead of our
    recovery script solution
    
    Change-Id: I9c65aac816f88775a80b3dd9cba459c031656491

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -1029,6 +1029,16 @@ public class Build {
     public static final String FINGERPRINT = deriveFingerprint();
 
     /**
+     * TOXYC fingerprint
+     * @hide
+     */
+    public static final String PROP_TOXYC_FINGERPRINT = "com.toxyc.fingerprint";
+
+    /** @hide */
+    public static final String TOXYC_FINGERPRINT = SystemProperties.get(PROP_TOXYC_FINGERPRINT,
+            deriveFingerprint());
+
+    /**
      * Some devices split the fingerprint components between multiple
      * partitions, so we might derive the fingerprint at runtime.
      */
