31ef3bef636e5a2e9661aa52140b91bc076bc632
Mazda-- <alex@dirtyunicorns.com>
2020-01-20 21:24:30 +0530

    Themes: Add DU themes [1/3]
    - This adds 'solarized dark' theme
    
    Thanks to bigrushdog <randall.rushing@gmail.com> for helping me figure out
    how to apply different actions based on the item selected, really HUGE help!
    
    Signed-off-by: iamtoxyc <vtechnical1804@gmail.com>

--- core/java/com/android/internal/util/toxyc/ThemesUtils.java
+++ core/java/com/android/internal/util/toxyc/ThemesUtils.java
@@ -1,13 +1,11 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!--
-/**
- * Copyright (c) 2020, ZenX OS Project
+/*
+ * Copyright (C) 2014 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,9 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
--->
-<resources xmlns:xliff="urn:oasis:names:tc:xliff:document:1.2">
-    <!-- Atomos icon shape overlay -->
-    <string name="icon_shape_mallow_overlay" translatable="false">Mallow</string>
 
-</resources>
+package com.android.internal.util.toxyc;
+
+public class ThemesUtils {
+
+    public static final String[] SOLARIZED_DARK = {
+            "com.android.theme.solarizeddark.system",
+            "com.android.theme.solarizeddark.systemui",
+    };
+}
