7163cb444de789cd9c5e9a104143d17bb1d7117c
varund7726 <date.varun123@gmail.com>
2020-10-05 04:35:32 +0200

    base: Option to Display Data Disabled Indicator Icon [1/2]
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4822,6 +4822,12 @@ public final class Settings {
         public static final String MULTI_AUDIO_FOCUS_ENABLED = "multi_audio_focus_enabled";
 
         /**
+         * Whether to display cross sign for a data disabled connection
+         * @hide
+         */
+        public static final String DATA_DISABLED_ICON = "data_disabled_icon";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
